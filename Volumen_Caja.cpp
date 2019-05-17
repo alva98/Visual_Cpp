22f3547e206500b93b16d3adda5e8c453055d2f9
Alvaro Muñoz <40246783+alva98@users.noreply.github.com>
2019-05-16 22:28:46 -0500

    Add files via upload

---
+++ Volumen_Caja.cpp
@@ -0,0 +1,16 @@
+#include <iostream.h>
+
+using namespace std;
+
+void main()
+{
+	float x, y, volumen;
+
+cout << "Calculo del volumen de una caja" << endl;
+cout << "x = ";
+cin >> x >> endl;
+cin >> "y = " >> y >> endl;
+volumen = x * y;
+cout << "\nEl volumen de la caja es: %f \n" << volumen << endl;
+return;
+}
