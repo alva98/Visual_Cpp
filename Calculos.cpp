90a053be987f92a1afd32ccccabe4bc1be372112
Alvaro Muñoz <40246783+alva98@users.noreply.github.com>
2019-05-27 21:41:42 -0500

    Calculos 1.1
    Se separo cálculo de hipotenusa, perimetro y area dentro de main a una nueva funcion: Calculo_Hipotenusa.

--- Calculos.cpp
+++ Calculos.cpp
@@ -1,29 +1,43 @@
-#include <iostream.h>
-#include <math.h>
+// Prueba1.cpp : Ejercicios elementales de programación. 
+// Calculo de la hipotenusa a partir de los catetos.
+
+#include "C:\Users\Docente\OneDrive\Repositorios\Visual_Cpp\INCLUDE\iostream.h"
+#include <C:\Users\Docente\OneDrive\Repositorios\Visual_Cpp\INCLUDE\MATH.H>
+#include "pch.h"
 
 void Calcula_Volumen(float largo, float ancho, float alto)
-  {
-  Calcula_Volumen = largo * ancho * alto;
-  cout << "\nCálculo del volumen" << endl;
-  cout << "V = " << Calcula_Volumen << endl;
-  return;
-  }
+{
+	float volumen = largo * ancho * alto;
+	cout << "\nCálculo del volumen" << endl;
+	cout << "V = " << volumen << endl;
+	return;
+}
+
+void Calcula_Hipotenusa(float catetoA, float catetoB)
+{
+	float hipotenusa, perimetro = 0, area = 0;
+
+	cout << "\nCalculos de la hipotenusa, perimetro y area del triangulo rectangulo." << endl;
+	hipotenusa = (float)sqrt(pow(catetoA, 2) + pow(catetoB, 2));
+	perimetro = catetoA + catetoB + hipotenusa;
+	area = (catetoA*catetoB) / 2;
+	cout << "\n\nHipotenusa = " << hipotenusa;
+	cout << "\nPerimetro = " << perimetro;
+	cout << "\nArea = " << area << endl;
+	return;
+}
 
 int main()
 {
-float catetoA, catetoB, hipotenusa;
-float perimetro=0, area=0;
+	float ancho, largo, alto;
 
-cout << "Cateto A: ";
-cin >> catetoA;
-
-cout << "Cateto B: ";
-cin >> catetoB;
-
-hipotenusa = (float) sqrt(pow(catetoA, 2)+ pow(catetoB, 2));
-cout ;
-cout << "\n\nHipotenusa = " << hipotenusa;
-cout << "\n\n\n\n";
-
-return 0;
+	cout << "Entrada de las medidas de los lados (catetos) de un rectangulo" << endl;
+	cout << "Largo (Cateto A): ";
+	cin >> catetoA;
+	cout << "   Ancho (Cateto B): ";
+	cin >> catetoB;
+	Calcula_Hipotenusa(largo, ancho);
+	Calcula_Volumen(largo, ancho, alto);
+	return 0;
 }
+
